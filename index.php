6986745a55c218c1e6deaae9f45d78e89251d314
Praisecodes <cmancoder@gmail.com>
2022-06-09 00:10:24 +0100

    ...

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 ?>
--- login/index.php
+++ login/index.php
@@ -3,6 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
     require_once "./connection.php";
 
