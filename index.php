ec9c956c2fd657248557952994c07d671e7c1db7
Praisecodes <cmancoder@gmail.com>
2022-06-09 00:06:41 +0100

    Update: Trying All Possible Fixes

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Credentials: true");
+?>
