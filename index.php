e6a3df54ddefa10fc7877d84e1f414c49fa931d0
Praisecodes <cmancoder@gmail.com>
2022-06-09 00:14:59 +0100

    ...

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    // header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 ?>
--- login/index.php
+++ login/index.php
@@ -2,8 +2,8 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    //header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 
     require_once "./connection.php";
 
