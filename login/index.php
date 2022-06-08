205470011e5c675888555302a81176d37c988eb5
Praisecodes <cmancoder@gmail.com>
2022-06-09 00:04:48 +0100

    Update: Working on API

--- login/index.php
+++ login/index.php
@@ -2,6 +2,7 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Credentials: true");
 
     require_once "./connection.php";
 
