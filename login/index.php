b96c748224a10f5dada1c083125bfb05bc801452
Praisecodes <cmancoder@gmail.com>
2022-06-09 01:10:40 +0100

    ...

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
     // header("Access-Control-Allow-Headers: Content-Type");
-    //header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Credentials: true");
     header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
